dcb31c6ac7777d14638d9681fd1a514fc667f95c
dkiri <dkiri@esailors.de>
2017-12-13 16:45:38 +0000

    Ionic 3 sample app. Initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,25 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { FIREBASE_CONFIG } from './firebase.credentials';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   providers: [
     StatusBar,
---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBP12Z9URlss8s9wkJvySzmi0uHNdDUnTQ",
+  authDomain: "shoppinglist-206fc.firebaseapp.com",
+  databaseURL: "https://shoppinglist-206fc.firebaseio.com",
+  projectId: "shoppinglist-206fc",
+  storageBucket: "",
+  messagingSenderId: "376399783857"
+};
